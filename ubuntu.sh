2478908a1558bde8494218c5aa8fdfedadaaf86b
sneepzity <148448122+sneepzity@users.noreply.github.com>
2024-10-17 22:39:55 +0800

    ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -96,11 +96,12 @@ echo 'Make Ubuntu faster? (1/2)'
 read input
 if [ "$input" -gt 0 -a "$input" -lt 2 ]
 # Preload loads commonly used apps into RAM and apt-fast uses parallel (multiple) downloads to make apt faster
+# Also removing a few useless applications like thunderbird, libreoffice, vim (sorry vim users) and the gnome games
 then
 sudo apt-add-repository ppa:apt-fast/stable -y
 sudo apt update -y 
 sudo apt install -y preload apt-fast
-sudo apt remove *thunderbird* *libreoffice* *vim* aisleriot gnome-mahjongg gnome-mines gnome-sudoku 
+sudo apt remove *thunderbird* *libreoffice* *vim* aisleriot gnome-mahjongg gnome-mines gnome-sudoku  brltty duplicity empathy empathy-common example-content gnome-accessibility-themes gnome-contacts gnome-mines gnome-orca gnome-screensaver gnome-sudoku gnome-video-effects landscape-common libsane libsane-common python3-uno rhythmbox* sane-utils shotwell* telepathy* totem*  printer-driver-brlaser printer-driver-foo2zjs printer-driver-foo2zjs-common printer-driver-m2300w printer-driver-ptouch  printer-driver-splix # This may not work, subject to change
 else
 echo 'Thanks for looking at my scripts!'
 fi
