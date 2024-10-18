c60ce10ec8333ce1f0173aa1611e21b016aff2b8
sneepzity <148448122+sneepzity@users.noreply.github.com>
2024-10-18 15:27:27 +0800

    auto-cpufreq.sh

--- auto-cpufreq.sh
+++ auto-cpufreq.sh
@@ -3,9 +3,9 @@
 echo "This script installs auto-cpufreq on your system."
 
 echo "
-Install auto-cpufreq? (1/2)"
+Install auto-cpufreq? ( 1 for install / 2 for no )"
 read input
-if [ "$input" -gt 0 -a "$input" -lt 2 ]
+if [ "$input" -eq 1 ]
 then
 cd ~
 git clone https://github.com/AdnanHodzic/auto-cpufreq.git
