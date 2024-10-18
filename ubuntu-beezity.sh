5f5296903e442581af6c369584c4a80b86cfc2c2
sneepzity <148448122+sneepzity@users.noreply.github.com>
2024-10-18 14:20:28 +0800

    ubuntu-beezity.sh

--- ubuntu-beezity.sh
+++ ubuntu-beezity.sh
@@ -10,7 +10,7 @@ echo 'Do you want to install Playonlinux? (1/2)'
 read input
 if [ "$input" -gt 0 -a "$input" -lt 2 ]
 then
-sudo apt install playonlinux &> /dev/null
+sudo apt-get install playonlinux > /dev/null
 
 echo "Do you want to install VS Code, ncspot, Adobe Reader, 
 Space Cadet Pinball, Vesktop, VLC, Flatseal, Sober and Gnome extension-manager?
@@ -24,12 +24,13 @@ flatpak install -y --user --noninteractive com.adobe.Reader
 flatpak install -y --user --noninteractive com.github.k4zmu2a.spacecadetpinball 
 flatpak install -y --user --noninteractive dev.vencord.Vesktop 
 flatpak install -y --user --noninteractive org.videolan.VLC 
-flatpak install -y --user --noninteractive com.visualstudio.code
+flatpak install -y --user --noninteractive com.visualstudio.code 
 flatpak install -y --user --noninteractive com.github.tchx84.Flatseal 
 flatpak install -y --user --noninteractive us.zoom.Zoom 
-flatpak install -y --user --noninteractive com.mattjakeman.ExtensionManager
+flatpak install -y --user --noninteractive com.mattjakeman.ExtensionManager 
 flatpak install -y --user --noninteractive https://sober.vinegarhq.org.sober.flatpakref 
 flatpak install -y --user --noninteractive io.github.flattool.Warehouse
+echo 'Flatpak apps installed!'
 else
 echo 'Thanks for looking at my scripts!'
 fi
@@ -46,18 +47,18 @@ echo 'PATH="/home/$USER/bin:$PATH"' >> ~/.bashrc
 cd bin
 # These are the apps themselves, the output is directed towards a file and the output is the command to run flatpak apps
 'Creating apps...'
-echo 'flatpak run io.github.hrkfdn.ncspot' >> ncspot &> /dev/null
-echo 'flatpak run com.adobe.Reader' >> adobe-reader &> /dev/null
-echo 'flatpak run com.github.k4zmu2a.spacecadetpinball' >> spacecadetpinball &> /dev/null
-echo 'flatpak run dev.Vencord.Vesktop' >> vesktop &> /dev/null
-echo 'flatpak run org.videolan.VLC' >> vlc &> /dev/null
-echo 'flatpak run com.visualstudio.code' >> vscode &> /dev/null
-echo 'flatpak run com.github.tchx84.Flatseal' >> flatseal &> /dev/null
-echo 'flatpak run us.zoom.Zoom' >> zoom &> /dev/null
-echo 'flatpak run org.vinegarhq.Sober' >> sober &> /dev/null
-echo 'flatpak run org.vinegarhq.Sober' >> roblox &> /dev/null
-echo 'flatpak run com.mattjakeman.ExtensionManager' >> extension-manager &> /dev/null
-echo 'flatpak run io.github.flattool.Warehouse' >> warehouse &> /dev/null
+echo 'flatpak run io.github.hrkfdn.ncspot' >> ncspot > /dev/null
+echo 'flatpak run com.adobe.Reader' >> adobe-reader > /dev/null
+echo 'flatpak run com.github.k4zmu2a.spacecadetpinball' >> spacecadetpinball > /dev/null
+echo 'flatpak run dev.Vencord.Vesktop' >> vesktop > /dev/null
+echo 'flatpak run org.videolan.VLC' >> vlc > /dev/null
+echo 'flatpak run com.visualstudio.code' >> vscode > /dev/null
+echo 'flatpak run com.github.tchx84.Flatseal' >> flatseal > /dev/null
+echo 'flatpak run us.zoom.Zoom' >> zoom > /dev/null
+echo 'flatpak run org.vinegarhq.Sober' >> sober > /dev/null
+echo 'flatpak run org.vinegarhq.Sober' >> roblox > /dev/null
+echo 'flatpak run com.mattjakeman.ExtensionManager' >> extension-manager > /dev/null
+echo 'flatpak run io.github.flattool.Warehouse' >> warehouse > /dev/null
 # chmod +x makes them executables instead of just plain text files
 chmod +x ncspot
 chmod +x adobe-reader
@@ -72,18 +73,22 @@ chmod +x roblox
 chmod +x extension-manager
 Chmod +x warehouse
 cd
-echo 'Make sure to check the commands inside the bin folder using a terminal text-editor like vim or nano as they might be wrong and also subject to change which both can cause breakage!'
+echo 'Flatpak shortcuts created!'
+sleep 2
+echo "Make sure to check the commands inside the bin folder using a terminal text-editor like vim or nano as 
+they might be wrong and also subject to change which both can cause breakage!"
 else
 echo "Thanks for looking at my scripts!"
 fi
 
 echo "Set Gnome to dark mode, 
 performance mode, 
-12 hour idle and clock format, 
+12 hour idle,
+12 hour clock format, 
 no idle dimming, 
 no mouse acceleration, 
 do not disturb, 
-show seconds in clock and 
+show seconds in clock, and 
 install gnome-tweaks? (1/2)"
 read input
 if [ "$input" -gt 0 -a "$input" -lt 2 ]
@@ -95,7 +100,8 @@ gsettings set org.gnome.desktop.session idle-delay 86400
 gsettings set org.gnome.desktop.peripherals.mouse accel-profile 'flat'
 gsettings set org.gnome.desktop.notifications show-banners false
 gsettings set org.gnome.desktop.interface clock-show-seconds true
-sudo apt install gnome-tweaks gnome-shell-extensions chrome-gnome-shell &> /dev/null 
+sudo apt-get install gnome-tweaks gnome-shell-extensions chrome-gnome-shell > /dev/null
+echo 'Gnome tweaks applied!'
 else
 echo "Thanks for looking at my scripts!"
 fi
@@ -104,9 +110,10 @@ echo 'Install Custom Ubuntu and Debian ISO Creator (CUBIC)?(1/2)'
 read input
 if [ "$input" -gt 0 -a "$input" -lt 2 ]
 then
-sudo apt-add-repository ppa:cubic-wizard/release -y &> /dev/null
-sudo apt update -y &> /dev/null 
-sudo apt install -y --no-install-recommends cubic &> /dev/null
+sudo apt-get-add-repository ppa:cubic-wizard/release -y > /dev/null
+sudo apt-get update -y > /dev/null 
+sudo apt-get install -y --no-install-recommends cubic > /dev/null
+echo 'Cubic installed!'
 else
 echo "Thanks for looking at my scripts!"
 fi
@@ -117,15 +124,16 @@ read input
 if [ "$input" -gt 0 -a "$input" -lt 2 ]
 then
 # More advisable to remove one snap at a time and already assuming you removed firefox and snap-store with ubuntu.sh
-echo 'Close all snap apps before uninstalling if you have any installed'
-sudo snap remove --purge gtk-common-themes &> /dev/null
-sudo snap remove --purge gnome-42-2204 &> /dev/null
-sudo snap remove --purge snapd-desktop-integration &> /dev/null
-sudo snap remove --purge core22 &> /dev/null
-sudo snap remove --purge bare &> /dev/null
+echo 'Close all snap apps before uninstalling if you have any installed!'
+sudo snap remove --purge gtk-common-themes > /dev/null
+sudo snap remove --purge gnome-42-2204 > /dev/null
+sudo snap remove --purge snapd-desktop-integration > /dev/null
+sudo snap remove --purge core22 > /dev/null
+sudo snap remove --purge bare > /dev/null
 sudo rm -rf /var/cache/snapd
-sudo apt autoremove --purge snapd &> /dev/null
+sudo apt-get autoremove -y --purge snapd > /dev/null
 rm -rf ~/snap
+echo 'Snap removed entirely!'
 else
 echo "Thanks for looking at my scripts!"
 fi
@@ -136,9 +144,11 @@ if [ "$input" -gt 0 -a "$input" -lt 2 ]
 then
 # Install required dependencies 
 echo 'Installing dependencies...'
-sudo apt install meson libsystemd-dev pkg-config ninja-build git libdbus-1-dev libinih-dev build-essential &> /dev/null
+sudo apt-get install meson libsystemd-dev pkg-config ninja-build git libdbus-1-dev libinih-dev build-essential > /dev/null
 echo 'Cloning repository and installing gamemode via ./bootstrap.sh...'
-git clone https://github.com/FeralInteractive/gamemode.git &> /dev/null
+cd ~
+git clone https://github.com/FeralInteractive/gamemode.git > /dev/null
+echo 'Now installing gamemode script...'
 cd gamemode
 ./bootstrap.sh
 echo 'Run gamemoded -t to see if it insalled correctly!'
