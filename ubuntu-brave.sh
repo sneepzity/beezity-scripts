2df092886591720a8495c9af886291cb9474ec5c
sneepzity <148448122+sneepzity@users.noreply.github.com>
2024-10-18 15:31:12 +0800

    ubuntu-brave.sh

--- ubuntu-brave.sh
+++ ubuntu-brave.sh
@@ -1,5 +1,17 @@
-sudo apt-get install -y curl 
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+#!/bin/bash
+
+echo 'question? ( 1 for install / 2 for no )'
+read input
+if [ "$input" -eq 1 ]
+then
+echo 'Installing brave browser...'
+sudo apt-get install -y curl > /dev/null
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg > /dev/null
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt-get update -y
-sudo apt-get install -y brave-browser
+sudo apt-get update -y > /dev/null
+sudo apt-get install -y brave-browser > /dev/null
+else
+echo "Thanks for looking at my scripts!"
+fi
+
+exit
