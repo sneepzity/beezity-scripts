b8b675995f6a3122d74123dfb1718a6428865deb
sneepzity <148448122+sneepzity@users.noreply.github.com>
2024-10-18 15:26:22 +0800

    ubuntu-24.04-jakoolit-hyprland.sh

--- ubuntu-24.04-jakoolit-hyprland.sh
+++ ubuntu-24.04-jakoolit-hyprland.sh
@@ -1,7 +1,7 @@
 echo "
-Install JaKooLit's Hyprland Config? [https://github.com/JaKooLit/Ubuntu-Hyprland/tree/24.04] (1/2)"
+Install JaKooLit's Hyprland Config? [https://github.com/JaKooLit/Ubuntu-Hyprland/tree/24.04] ( 1 for install / 2 for no )"
 read input
-if [ "$input" -gt 0 -a "$input" -lt 2 ]
+if [ "$input" -eq 1 ]
 then
 git clone -b 24.04 --depth 1 https://github.com/JaKooLit/Ubuntu-Hyprland.git ~/Ubuntu-Hyprland-24.04
 cd ~/Ubuntu-Hyprland-24.04
