a97be9ee7d5e2fde1b817036714f500f3f817d47
sneepzity <148448122+sneepzity@users.noreply.github.com>
2024-10-18 14:15:26 +0800

    ubuntu-brave.sh

--- ubuntu-brave.sh
+++ ubuntu-brave.sh
@@ -1,5 +1,5 @@
-sudo apt install -y curl 
+sudo apt-get install -y curl 
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update -y
-sudo apt install -y brave-browser
+sudo apt-get update -y
+sudo apt-get install -y brave-browser
