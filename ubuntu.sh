58f8737511748ae2607eafcb9c6eb9bca4a76a1c
sneepzity <148448122+sneepzity@users.noreply.github.com>
2024-10-19 17:10:25 +0800

    ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -29,7 +29,7 @@ sudo mkdir -pm755 /etc/apt/keyrings
 echo 'Now installing brave browser...'
 sleep 2
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg 
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" > /dev/null | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt-get update -y > /dev/null
 sudo apt-get install -y brave-browser > /dev/null
 echo 'Brave installed!'
