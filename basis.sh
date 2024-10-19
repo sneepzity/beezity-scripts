83afffb9f28cdfb1408911e978142ea14d3c71d2
sneepzity <148448122+sneepzity@users.noreply.github.com>
2024-10-19 20:16:15 +0800

    basis.sh

--- basis.sh
+++ basis.sh
@@ -17,3 +17,38 @@ else
 # echo 'Continuing!'
 # fi
 fi
+
+echo 'x?' ( 1 for yes / 2 for no )
+read input
+if [ "$input" -eq 1 ]
+then
+command
+else
+# command 2
+fi
+
+echo 'x?' ( 1 for yes / 2 for no )
+read input
+if [ "$input" -eq 1 ]
+then
+command
+else
+echo 'Continuing!' 
+fi
+
+echo 'x?' ( 1 for yes / 2 for no )
+read input
+if [ "$input" -eq 1 ]
+then
+command
+else
+echo 'Do you want to leave? ( 1 to leave / 2 to continue )'
+read input
+if [ "$input" -eq 1 ]
+then
+echo 'Thanks for looking at my scripts!'
+exit
+else
+echo 'Continuing!'
+fi
+fi
