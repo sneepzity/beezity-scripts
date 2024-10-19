c3d98247b6ba166a4fd0295fe540672290acfdbc
sneepzity <148448122+sneepzity@users.noreply.github.com>
2024-10-19 20:11:42 +0800

    Add files via upload

---
+++ basis.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo 'x?' ( 1 for yes / 2 for no )
+read input
+if [ "$input" -eq 1 ]
+then
+command
+else
+# echo 'Continuing!' 
+# echo 'Do you want to leave? ( 1 to leave / 2 to continue )'
+# read input
+# if [ "$input" -eq 1 ]
+# then
+# echo 'Thanks for looking at my scripts!'
+# exit
+# else
+# echo 'Continuing!'
+# fi
+fi
