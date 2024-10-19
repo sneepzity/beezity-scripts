8791461e2a785c46d699755973184e9d14199cbe
sneepzity <148448122+sneepzity@users.noreply.github.com>
2024-10-19 17:21:35 +0800

    ubuntu-24.04.sh

--- ubuntu-24.04.sh
+++ ubuntu-24.04.sh
@@ -83,8 +83,8 @@ echo 'Wine installed!'
 echo 'Now installing firefox...'
 wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
 echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
-sudo apt-get update
-sudo apt-get install firefox
+sudo apt-get update -y
+sudo apt-get install firefox -y
 echo 'Firefox deb version installed!'
 # This installs flatpak, a much better alternative to snap with much wider app support and compatibility
 echo 'Now installing flatpak and flathub..'
