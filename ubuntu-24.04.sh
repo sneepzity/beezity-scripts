b5ff4b26fc0d6f268f70a4e8e32155d2072c62d7
sneepzity <148448122+sneepzity@users.noreply.github.com>
2024-10-19 17:05:11 +0800

    ubuntu-24.04.sh

--- ubuntu-24.04.sh
+++ ubuntu-24.04.sh
@@ -28,7 +28,7 @@ sudo mkdir -pm755 /etc/apt/keyrings
 # The four commands below install brave browser, comment it out if you don't want to install brave
 echo 'Now installing brave browser...'
 sleep 2
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg > /dev/null
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt-get update -y > /dev/null
 sudo apt-get install -y brave-browser > /dev/null
