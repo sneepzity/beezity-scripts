6cdc47d514ee56a96e4fe9991907ad7defe36600
sneepzity <148448122+sneepzity@users.noreply.github.com>
2024-10-20 11:46:32 +0800

    ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -174,6 +174,14 @@ if [ "$input" -eq 1 ]
 then
 wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
 echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+echo '
+Package: *
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+
+Package: firefox*
+Pin: release o=Ubuntu
+Pin-Priority: -1' | sudo tee /etc/apt/preferences.d/mozilla
 echo 'Now installing firefox...'
 sudo apt-get update -y > /dev/null
 sudo apt-get install firefox -y > /dev/null
