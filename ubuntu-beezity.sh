528f02d4c8967743271b39862732054ae88a1bc9
beezity <148448122+sneepzity@users.noreply.github.com>
2024-10-24 01:23:42 +0800

    ubuntu-beezity.sh

--- ubuntu-beezity.sh
+++ ubuntu-beezity.sh
@@ -29,7 +29,7 @@ read input
 if [ "$input" -eq 1 ]
 then
 sudo apt-get update -y
-sudo apt-get install -y playonlinux winbind libxcb-cursor0 konsole > /dev/null
+sudo apt-get install -y playonlinux winbind > /dev/null
 else
 echo 'Do you want to leave? ( 1 to leave / 2 to continue )'
 read input
@@ -512,7 +512,7 @@ sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyring
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get -y update
-sudo apt-get install -y pass uidmap libqrencod4 qrencode tree xclip
+sudo apt-get install -y pass uidmap libqrencod4 qrencode tree xclip libxcb-cursor0 konsole > /dev/null
 sudo systemctl enable --now docker.service
 sudo systemctl enable --now containerd.service
 cd /home/$USER/beezity-scripts
