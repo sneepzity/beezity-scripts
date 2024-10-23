b4f413710cc1fa3b6572ea5ce62ba37cf9f7776d
beezity <148448122+sneepzity@users.noreply.github.com>
2024-10-23 12:00:22 +0800

    ubuntu-beezity.sh

--- ubuntu-beezity.sh
+++ ubuntu-beezity.sh
@@ -571,6 +571,21 @@ sudo adduser $USER libvirt
 sudo systemctl restart libvirtd
 sudo virsh net-start default
 sudo systemctl enable --now libvirtd && systemctl status libvirtd
+curl -sSL https://get.docker.com | sh
+sudo usermod -aG docker $USER
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo 
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu 
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo systemctl enable --now docker.service
+sudo systemctl enable --now containerd.service
+cd /home/$USER/beezity-scripts
+wget https://desktop.docker.com/linux/main/amd64/docker-desktop-amd64.deb
+sudo dpkg -i docker-desktop-amd64.deb
 cd /home/$USER/Downloads
 echo 'Aria2c or wget? ( 1 for aria2 / 2 for wget )'
 read input
