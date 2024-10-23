7ad936e48996fd8b25a763aed2c9f43023900ece
beezity <148448122+sneepzity@users.noreply.github.com>
2024-10-23 10:34:45 +0800

    ubuntu-beezity.sh

--- ubuntu-beezity.sh
+++ ubuntu-beezity.sh
@@ -88,9 +88,9 @@ then
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo apt-get -y install apt-transport-https
-sudo apt-get -y update 
-sudo apt-get -y install code
+sudo apt-get -y install apt-transport-https > /dev/null
+sudo apt-get -y update > /dev/null
+sudo apt-get -y install code > /dev/null
 mkdir -p /home/$USER/bin
 cd /home/$USER/bin
 echo 'code' >> VSCode
