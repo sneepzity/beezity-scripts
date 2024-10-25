c70d4e59d236d00b08d639479a258ce8df55f3a6
beezity <148448122+sneepzity@users.noreply.github.com>
2024-10-25 12:32:53 +0800

    ubuntu-beezity.sh

--- ubuntu-beezity.sh
+++ ubuntu-beezity.sh
@@ -827,6 +827,23 @@ echo 'Continuing!'
 fi
 fi
 
+echo 'Install Nala? (An APT alternative) ( 1 for yes / 2 for no )'
+read input
+if [ "$input" -eq 1 ]
+then
+curl https://gitlab.com/volian/volian-archive/-/raw/main/install-nala.sh | bash
+else
+echo 'Do you want to leave? ( 1 to leave / 2 to continue )'
+read input
+if [ "$input" -eq 1 ]
+then
+echo 'Thanks for looking at my scripts!'
+exit
+else
+echo 'Continuing!'
+fi
+fi
+
 echo 'Update all packages (Flatpak and APT)? ( 1 for yes / 2 for no )'
 read input
 if [ "$input" -eq 1 ]
