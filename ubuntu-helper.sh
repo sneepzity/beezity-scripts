04df98292c5d67c03abff8fa551263d51811c436
beezity <148448122+sneepzity@users.noreply.github.com>
2025-01-18 21:10:30 +0800

    ubuntu-helper.sh

--- ubuntu-helper.sh
+++ ubuntu-helper.sh
@@ -8,16 +8,10 @@ echo 'Now are you gonna use ubuntu or ubuntu-beezity? ( 1 for ubuntu / 2 for ubu
 read input
 if [ "$input" -eq 1 ]
 then
-cd /home/$USER
-mkdir -p /home/$USER/beezity-scripts
-cd beezity-scripts
 wget https://raw.githubusercontent.com/sneepzity/beezity-scripts/refs/heads/main/ubuntu.sh
 chmod +x *.sh
 ./ubuntu.sh
 else
-cd /home/$USER
-mkdir -p /home/$USER/beezity-scripts
-cd beezity-scripts
 wget https://raw.githubusercontent.com/sneepzity/beezity-scripts/refs/heads/main/ubuntu-beezity.sh
 chmod +x *.sh
 ./ubuntu-beezity.sh
