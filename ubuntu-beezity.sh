c35ebeaf4e3ba8d364bbc0cb24ea9a1e1cfb883d
beezity <148448122+sneepzity@users.noreply.github.com>
2025-01-19 15:08:04 +0800

    ubuntu-beezity.sh

--- ubuntu-beezity.sh
+++ ubuntu-beezity.sh
@@ -997,7 +997,7 @@ sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyring
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get -y update
-sudo apt-get install -y pass uidmap libqrencod4 qrencode tree xclip libxcb-cursor0 konsole > /dev/null
+sudo apt-get install -y pass uidmap libqrencode4 qrencode tree xclip libxcb-cursor0 konsole > /dev/null
 sudo systemctl enable --now docker.service
 sudo systemctl enable --now containerd.service
 cd /home/$USER/beezity-scripts
