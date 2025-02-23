bbb106c5ebbcc1166352eedcdac56f6d45b376c3
beezity <148448122+sneepzity@users.noreply.github.com>
2025-02-23 18:40:59 +0800

    ubuntu-helper.sh

--- ubuntu-helper.sh
+++ ubuntu-helper.sh
@@ -1,28 +1,7 @@
 #!/bin/bash
 
-#echo 'Ubuntu helper script, so you dont have to clone the whole repo'
-#sleep 5
-#clear
-
-#echo 'Now are you gonna use ubuntu or ubuntu-beezity? ( 1 for ubuntu / 2 for ubuntu-beezity )'
-#read input
-#if [ "$input" -eq 1 ]
-#then
-#wget https://raw.githubusercontent.com/sneepzity/beezity-scripts/refs/heads/main/ubuntu.sh
-#chmod +x *.sh
-#./ubuntu.sh
-#else
-#wget https://raw.githubusercontent.com/sneepzity/beezity-scripts/refs/heads/main/ubuntu-beezity.sh
-#chmod +x *.sh
-#./ubuntu-beezity.sh
-#fi
-
-#exit
-
-#!/bin/bash
-
 echo 'Ubuntu helper script, so you dont have to clone the whole repo'
-sleep 5
+sleep 3
 clear
 
 echo 'Now are you gonna use ubuntu or ubuntu-beezity? ( 1 for ubuntu / 2 for ubuntu-beezity )'
