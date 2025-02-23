3fd5403064891dbedc03776f433a99aaf6ffc66d
beezity <148448122+sneepzity@users.noreply.github.com>
2025-02-23 18:39:43 +0800

    ubuntu-helper.sh

--- ubuntu-helper.sh
+++ ubuntu-helper.sh
@@ -1,20 +1,43 @@
 #!/bin/bash
 
+#echo 'Ubuntu helper script, so you dont have to clone the whole repo'
+#sleep 5
+#clear
+
+#echo 'Now are you gonna use ubuntu or ubuntu-beezity? ( 1 for ubuntu / 2 for ubuntu-beezity )'
+#read input
+#if [ "$input" -eq 1 ]
+#then
+#wget https://raw.githubusercontent.com/sneepzity/beezity-scripts/refs/heads/main/ubuntu.sh
+#chmod +x *.sh
+#./ubuntu.sh
+#else
+#wget https://raw.githubusercontent.com/sneepzity/beezity-scripts/refs/heads/main/ubuntu-beezity.sh
+#chmod +x *.sh
+#./ubuntu-beezity.sh
+#fi
+
+#exit
+
+#!/bin/bash
+
 echo 'Ubuntu helper script, so you dont have to clone the whole repo'
 sleep 5
 clear
 
 echo 'Now are you gonna use ubuntu or ubuntu-beezity? ( 1 for ubuntu / 2 for ubuntu-beezity )'
 read input
-if [ "$input" -eq 1 ]
-then
-wget https://raw.githubusercontent.com/sneepzity/beezity-scripts/refs/heads/main/ubuntu.sh
-chmod +x *.sh
-./ubuntu.sh
+
+if [ "$input" -eq 1 ]; then
+    wget https://raw.githubusercontent.com/sneepzity/beezity-scripts/refs/heads/main/ubuntu.sh
+    chmod +x *.sh
+    ./ubuntu.sh
+elif [ "$input" -eq 2 ]; then
+    wget https://raw.githubusercontent.com/sneepzity/beezity-scripts/refs/heads/main/ubuntu-beezity.sh
+    chmod +x *.sh
+    ./ubuntu-beezity.sh
 else
-wget https://raw.githubusercontent.com/sneepzity/beezity-scripts/refs/heads/main/ubuntu-beezity.sh
-chmod +x *.sh
-./ubuntu-beezity.sh
+    echo "Invalid input. Please enter 1 or 2."
 fi
 
 exit
