277a5d6d74b23d00cb2d20678c745dbc06b07e0e
beezity <148448122+sneepzity@users.noreply.github.com>
2025-02-23 18:46:34 +0800

    ubuntu-helper.sh

--- ubuntu-helper.sh
+++ ubuntu-helper.sh
@@ -11,10 +11,12 @@ if [ "$input" -eq 1 ]; then
     sudo apt-get -y install curl > /dev/null
     mkdir -p /home/$USER/beezity-scripts && cd /home/$USER/beezity-scripts
     curl -sSL https://raw.githubusercontent.com/sneepzity/beezity-scripts/refs/heads/main/ubuntu.sh | bash
+    exit
 elif [ "$input" -eq 2 ]; then
     sudo apt-get -y install curl > /dev/null
     mkdir -p /home/$USER/beezity-scripts && cd /home/$USER/beezity-scripts
     curl -sSL https://raw.githubusercontent.com/sneepzity/beezity-scripts/refs/heads/main/ubuntu-beezity.sh | bash
+    exit
 else
     echo "Invalid input. Please enter 1 or 2."
 fi
